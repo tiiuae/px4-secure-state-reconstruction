3a50be099ed01c85a8274fc9db5997af9f821207
Junior Sundar <junior.sundar@tii.ae>
2024-08-26 21:09:58 +0400

    Created new safe_controller node

--- px4_offboard_control/src/sensor_matrix_downsampler.cpp
+++ px4_offboard_control/src/sensor_matrix_downsampler.cpp
@@ -1,94 +1,103 @@
-#include <std_msgs/msg/float64_multi_array.hpp>
-#include <px4_offboard_control/msg/timestamped_array.hpp>
 #include <px4_msgs/msg/vehicle_local_position.hpp>
+#include <px4_offboard_control/msg/timestamped_array.hpp>
 #include <rclcpp/rclcpp.hpp>
+#include <std_msgs/msg/float64_multi_array.hpp>
 #include <stdint.h>
 
 #include <iostream>
 #include <string>
 
-class SensorMatrixDownsampler : public rclcpp::Node
-{
+class SensorMatrixDownsampler : public rclcpp::Node {
 public:
-	SensorMatrixDownsampler() : Node("sensor_matrix_downsampler")
-	{
-		rmw_qos_profile_t qos_profile = rmw_qos_profile_sensor_data;
-		auto qos = rclcpp::QoS(rclcpp::QoSInitialization(qos_profile.history, 5), qos_profile);
+    SensorMatrixDownsampler() : Node("sensor_matrix_downsampler") {
+        rmw_qos_profile_t qos_profile = rmw_qos_profile_sensor_data;
+        auto qos = rclcpp::QoS(rclcpp::QoSInitialization(qos_profile.history, 5),
+                               qos_profile);
 
-		sensor_matrix_publisher_ = this->create_publisher<px4_offboard_control::msg::TimestampedArray>("/sensor_matrices", 10);
+        sensor_matrix_publisher_ =
+            this->create_publisher<px4_offboard_control::msg::TimestampedArray>(
+                "/sensor_matrices", 10);
 
-        ekf_subscriber_1_ = this->create_subscription<px4_msgs::msg::VehicleLocalPosition>(
-            "/fmu/out/vehicle_local_position",
-            qos,
-            std::bind(&SensorMatrixDownsampler::ekf_callback_1, this, std::placeholders::_1)
-        );
-		ekf_subscriber_2_ = this->create_subscription<px4_msgs::msg::VehicleLocalPosition>(
-            "/fmu/out/vehicle_local_position/attacked",
-            qos,
-            std::bind(&SensorMatrixDownsampler::ekf_callback_2, this, std::placeholders::_1)
-        );
+        ekf_subscriber_1_ =
+            this->create_subscription<px4_msgs::msg::VehicleLocalPosition>(
+                "/fmu/out/vehicle_local_position", qos,
+                std::bind(&SensorMatrixDownsampler::ekf_callback_1, this,
+                          std::placeholders::_1));
+        ekf_subscriber_2_ =
+            this->create_subscription<px4_msgs::msg::VehicleLocalPosition>(
+                "/fmu/out/vehicle_local_position/attacked", qos,
+                std::bind(&SensorMatrixDownsampler::ekf_callback_2, this,
+                          std::placeholders::_1));
 
         sampling_freq = 20; // In Hertz
 
-		timer_ = this->create_wall_timer(
-			std::chrono::milliseconds(static_cast<long>(1/sampling_freq * 1000)),
-			std::bind(&SensorMatrixDownsampler::timerCallback, this));
-	}
+        timer_ = this->create_wall_timer(
+            std::chrono::milliseconds(static_cast<long>(1 / sampling_freq * 1000)),
+            std::bind(&SensorMatrixDownsampler::timerCallback, this));
+    }
 
 private:
-	px4_msgs::msg::VehicleLocalPosition ekf_1, ekf_2;
+    px4_msgs::msg::VehicleLocalPosition ekf_1, ekf_2;
     float sampling_freq;
 
-	rclcpp::Publisher<px4_offboard_control::msg::TimestampedArray>::SharedPtr sensor_matrix_publisher_;
-	rclcpp::Subscription<px4_msgs::msg::VehicleLocalPosition>::SharedPtr ekf_subscriber_1_, ekf_subscriber_2_;
+    rclcpp::Publisher<px4_offboard_control::msg::TimestampedArray>::SharedPtr
+    sensor_matrix_publisher_;
+    rclcpp::Subscription<px4_msgs::msg::VehicleLocalPosition>::SharedPtr
+    ekf_subscriber_1_,
+    ekf_subscriber_2_;
 
-	void ekf_callback_1(px4_msgs::msg::VehicleLocalPosition position);
-	void ekf_callback_2(px4_msgs::msg::VehicleLocalPosition position);
+    void ekf_callback_1(px4_msgs::msg::VehicleLocalPosition position);
+    void ekf_callback_2(px4_msgs::msg::VehicleLocalPosition position);
 
-	void timerCallback();
-	rclcpp::TimerBase::SharedPtr timer_;
+    void timerCallback();
+    rclcpp::TimerBase::SharedPtr timer_;
 };
 
 /*
- * @brief Repackage data from sensors into a sensor matrix and publish at predefined interval
-*/
-void SensorMatrixDownsampler::timerCallback()
-{
-	/*std_msgs::msg::Float64MultiArray output;*/
+ * @brief Repackage data from sensors into a sensor matrix and publish at
+ * predefined interval
+ */
+void SensorMatrixDownsampler::timerCallback() {
+    /*std_msgs::msg::Float64MultiArray output;*/
     px4_offboard_control::msg::TimestampedArray output;
-	std::vector<double> sensor_vector{static_cast<double>(ekf_1.x), static_cast<double>(ekf_1.vx), static_cast<double>(ekf_2.x), static_cast<double>(ekf_2.vx)};
-	/*output.data = sensor_vector;*/
-    output.header.stamp.sec = static_cast<int>(ekf_1.timestamp/1000000);
-    output.header.stamp.nanosec = static_cast<unsigned int>(static_cast<unsigned long>(ekf_1.timestamp)*1000 - static_cast<unsigned long>(output.header.stamp.sec)*1000000000);
-	output.array.data = sensor_vector;
-	sensor_matrix_publisher_->publish(output);
+    std::vector<double> sensor_vector{
+        static_cast<double>(ekf_1.x), static_cast<double>(ekf_1.vx),
+        static_cast<double>(ekf_1.y), static_cast<double>(ekf_1.vy),
+        static_cast<double>(ekf_2.x), static_cast<double>(ekf_2.vx),
+        static_cast<double>(ekf_2.y), static_cast<double>(ekf_2.vy)};
+    /*output.data = sensor_vector;*/
+    output.header.stamp.sec = static_cast<int>(ekf_1.timestamp / 1000000);
+    output.header.stamp.nanosec = static_cast<unsigned int>(
+        static_cast<unsigned long>(ekf_1.timestamp) * 1000 -
+        static_cast<unsigned long>(output.header.stamp.sec) * 1000000000);
+    output.array.data = sensor_vector;
+    sensor_matrix_publisher_->publish(output);
 }
 
 /**
  * @brief Obtain and store sensor output from one of the EKFs
  * @param position	Position output from the EKF
  */
-void SensorMatrixDownsampler::ekf_callback_1(px4_msgs::msg::VehicleLocalPosition position)
-{
-	this->ekf_1 = position;
+void SensorMatrixDownsampler::ekf_callback_1(
+    px4_msgs::msg::VehicleLocalPosition position) {
+    this->ekf_1 = position;
 }
 
 /**
  * @brief Obtain and store sensor output from one of the EKFs
  * @param position	Position output from the EKF
  */
-void SensorMatrixDownsampler::ekf_callback_2(px4_msgs::msg::VehicleLocalPosition position)
-{
-	this->ekf_2 = position;
+void SensorMatrixDownsampler::ekf_callback_2(
+    px4_msgs::msg::VehicleLocalPosition position) {
+    this->ekf_2 = position;
 }
 
-int main(int argc, char *argv[])
-{
-	std::cout << "Starting sensor_matrix_downsampler node..." << std::endl;
-	setvbuf(stdout, NULL, _IONBF, BUFSIZ);
-	rclcpp::init(argc, argv);
-	rclcpp::spin(std::make_shared<SensorMatrixDownsampler>());
+int main(int argc, char *argv[]) {
+    std::cout << "Starting sensor_matrix_downsampler node..." << std::endl;
+    setvbuf(stdout, NULL, _IONBF, BUFSIZ);
+    rclcpp::init(argc, argv);
+    rclcpp::spin(std::make_shared<SensorMatrixDownsampler>());
 
-	rclcpp::shutdown();
-	return 0;
+    rclcpp::shutdown();
+    return 0;
 }
---
+++ px4_ssr/px4_ssr/safe_controller.py
@@ -0,0 +1,245 @@
+import numpy as np
+from scipy import linalg
+from cvxopt import matrix, solvers
+from px4_ssr.drone_system import (
+    SSProblem,
+)
+
+
+class LinearInequalityConstr:
+    """
+    This class defines linear inequality constraints and methods including compare, visualize
+
+    linear inequality constraints: a_mat x + b_vec \geq 0 (or, -a_mat x \leq b_vec)
+    a: (M,N) array
+    b: (M,1) array
+    """
+
+    def __init__(self, a: np.ndarray, b: np.ndarray) -> None:
+        b = b.reshape(-1, 1)
+        assert a.shape[0] == b.shape[0]
+        self.a_mat = a
+        self.b_vec = b
+
+    def compare(self, linear_ineq_constr):
+        pass
+
+    def visualize(self, dim=(0, 1)):
+        pass
+
+    def combine(self, linear_ineq_constr):
+        a_mat = np.vstack((self.a_mat, linear_ineq_constr.a_mat))
+        b_vec = np.vstack((self.b_vec, linear_ineq_constr.b_vec))
+        return LinearInequalityConstr(a_mat, b_vec)
+
+    def is_satisfy(self, u):
+        u = u.reshape(-1, 1)
+        assert u.shape[0] == self.a_mat.shape[1]
+
+        return all(self.a_mat @ u + self.b_vec + 1e-6 >= 0)
+
+
+class SafeProblem:
+    """
+    This class defines data for safe problem
+    """
+
+    def __init__(self, ss_problem: SSProblem, h, q, gamma) -> None:
+        assert gamma >= 0
+        assert gamma <= 1
+
+        assert h.shape[0] == q.shape[0]
+        assert h.shape[1] == ss_problem.A.shape[1]
+        assert q.shape[1] == 1
+
+        self.problem = ss_problem
+
+        self.h = h
+        self.q = q
+        self.gamma = gamma
+
+        # according to (7) of the note "Safety of linear systems under sensor attacks without state estimation
+        self.k = (1 - gamma) * h @ np.linalg.matrix_power(
+            self.problem.A, self.problem.io_length
+        ) - h @ np.linalg.matrix_power(self.problem.A, self.problem.io_length + 1)
+
+    def cal_cbf_condition_state(self, state) -> LinearInequalityConstr:
+        state = state.reshape(-1, 1)
+        a_mat = self.h @ self.problem.B
+        b_vec = (
+            self.h @ self.problem.A @ state
+            + self.q
+            - (1 - self.gamma) * (self.h @ state + self.q)
+        )
+        return LinearInequalityConstr(a_mat, b_vec)
+
+    def merge_multiple_LIC(self, constr_list) -> LinearInequalityConstr:
+        lic_full = constr_list[0]
+        if len(constr_list) == 1:
+            return lic_full
+
+        for lic in constr_list[1:]:
+            lic_full = LinearInequalityConstr.combine(lic_full, lic)
+        return lic_full
+
+    def cal_cbf_condition(self, possible_states) -> LinearInequalityConstr:
+        """
+        This method calculates CBF conditions over a set of states
+        """
+        cbf_conditions = []
+        for state in possible_states:
+            cbf_condition = self.cal_cbf_condition_state(state)
+            cbf_conditions.append(cbf_condition)
+
+        total_cbf_condition = self.merge_multiple_LIC(cbf_conditions)
+        return total_cbf_condition
+
+    def cal_Q_ut(self):
+        """
+        construct Q(u(t)) according to (7) of the note "Safety of linear systems under sensor attacks without state estimation
+
+        """
+
+        H = self.h
+        q = self.q
+        gamma = self.gamma
+
+        A = self.problem.A
+        B = self.problem.B
+        io_length = self.problem.io_length
+        n = self.problem.n
+        m = self.problem.m
+
+        u_seq = self.problem.u_seq
+
+        left_mat = H @ ((1 - gamma) * np.identity(n) - A)
+        right_vec = np.zeros((n, 1))
+        for k in range(io_length - 1):
+            ut = u_seq[k, :]
+            ut = ut.reshape(-1, 1)
+            temp = (
+                linalg.fractional_matrix_power(A, io_length - 2 - k) @ B @ ut
+            )  # note in the paper, io_length = t + 1
+            right_vec = right_vec + temp
+
+        return left_mat @ right_vec - gamma * q
+
+    def cal_safe_input_constr_woSSR(
+        self, initial_states_subssr
+    ) -> LinearInequalityConstr:
+        """
+        This method implements our computationally efficient CBF conditions
+        initial_states_subssr is a list of possible_initial_states, each entry correspnds to one subspace
+        possible_initial_states = initial_states_subssr[j] is a n*x 2d array, each column corresponds to one state
+
+        """
+        Q_ut = self.cal_Q_ut()
+        kv_maxsum = np.zeros(np.shape(self.q))
+        count_subspace = len(initial_states_subssr)
+        for j in range(count_subspace):
+            possible_initial_states = initial_states_subssr[j]
+            kv = self.k @ possible_initial_states
+            kv_max = kv.max(axis=1)
+            kv_max = kv_max.reshape(-1, 1)
+            kv_maxsum = kv_maxsum + kv_max
+        # a_mat x + b_vec \geq 0
+        a_mat = self.h @ self.problem.B
+        b_vec = -kv_maxsum - Q_ut
+
+        # print(f'a_mat: {a_mat.shape}')
+        # print(f'b_vec: {b_vec.shape}')
+        # print(f'kv_maxsum: {kv_maxsum.shape}')
+        # print(f'Q_ut shape: {Q_ut}')
+        return LinearInequalityConstr(a_mat, b_vec)
+
+    def cal_safe_qp(self, u_nom, lic: LinearInequalityConstr):
+        """
+        sol_flag: -1 -- qp solver fails, 0 -- qp status "unknown", 1 -- qp status 'optimal'
+        """
+        flag = 0
+        # Define QP parameters for qp solver (CVXOPT):
+        # min 0.5 xT P x+ qT x s.t. Gx <= h
+        qp_P = matrix(np.identity(u_nom.shape[0]))
+        qp_q = matrix(-u_nom, (u_nom.shape[0], 1), "d")  #  qp_q is a m*1 matrix
+        qp_G = matrix(-lic.a_mat)
+        qp_h = matrix(lic.b_vec, (lic.b_vec.shape[0], 1), "d")  # qp_h is a x*1 matrix
+
+        solvers.options["show_progress"] = False  # mute optimization output
+        solvers.options["maxiters"] = 500  # increase max iteration number
+        solvers.options["abstol"] = 1e-4
+        solvers.options["reltol"] = 1e-5
+        try:
+            solv_sol = solvers.qp(qp_P, qp_q, qp_G, qp_h)
+            if solv_sol["status"] == "unknown":
+                print(
+                    "warning: safe control is approximately computed. Use u_nom instead."
+                )
+                u = u_nom.flatten()
+            else:
+                flag = 1
+                u = np.array(
+                    solv_sol["x"]
+                ).flatten()  # extract decision variables of optimization problem
+        except Exception as _:
+            # print('cvxopt solver failed:')
+            # print(f'qp_P: {qp_P}')
+            # print(f'qp_q: {qp_q}')
+            # print(f'qp_G: {qp_G}')
+            # print(f'qp_h: {qp_h}')
+
+            # solvers.options['show_progress'] = True
+            # solv_sol = solvers.qp(qp_P,qp_q,qp_G,qp_h)
+
+            # raise TypeError('no control input found')
+            print("warning: safe control fails. Use u_nom instead.")
+            flag = -1
+            u = u_nom.flatten()
+
+        return u, flag
+
+    def cal_safe_control(self, u_nom, possible_states):
+        lic = self.cal_cbf_condition(possible_states)
+        # print(f'lic.a_mat:\n {lic.a_mat}')
+        # print(f'lic.b_vec:\n {lic.b_vec}')
+        u, flag = self.cal_safe_qp(u_nom, lic)
+        return u, lic, flag
+
+    def max_kv(self):
+        possible_init_states, _, _ = self.ssr.solve_initial_state()
+        kv = self.k @ possible_init_states
+        kv_max = kv.max(axis=1)
+        return kv_max
+
+    def max_test_formula(self):
+        vec_list = []
+        for comb in self.ssr.possible_comb:
+            obser_matrix = self.ssr.construct_obser_matrix(comb)
+            measure_vec = self.ssr.construct_measurement_his(comb)
+            vec = np.linalg.pinv(obser_matrix) @ measure_vec
+            vec_list.append(vec)
+        vecs = np.hstack(vec_list)
+        tf = self.k @ vecs
+        tf_max = tf.max(axis=1)
+        return tf_max
+
+    def test2(self):
+        diff_list = []
+        for comb in self.ssr.possible_comb:
+            obser_matrix = self.ssr.construct_obser_matrix(comb)
+            measure_vec = self.ssr.construct_measurement_his(comb)
+            lhs = self.k @ np.linalg.pinv(obser_matrix) @ measure_vec
+
+            for sensor_i in range(self.problem.p):
+                if sensor_i not in comb:
+                    # Create a new combination by copying and extending the original combination
+                    comb_ext = comb.copy()
+                    comb_ext.append(sensor_i)
+                    obser_matrix_ext = self.ssr.construct_obser_matrix(comb_ext)
+                    measure_vec_ext = self.ssr.construct_measurement_his(comb_ext)
+                    rhs = self.k @ np.linalg.pinv(obser_matrix_ext) @ measure_vec_ext
+
+                    diff = lhs - rhs
+                    diff_list.append(diff)
+
+        diff_array = np.array(diff_list)
+        return diff_array
